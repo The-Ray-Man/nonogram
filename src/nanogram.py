9c5e7d8cdcfce700ef6a6f3c02158ff1f0c2b1c8
Gottschali <aligottschall22@gmail.com>
2021-05-17 21:28:15 +0200

    remove unnecesary padding

--- src/nanogram.py
+++ src/nanogram.py
@@ -9,6 +9,8 @@ SQUARE = 1
 CROSS = -1
 KEINE = 0
 
+# TODO verify function
+
 def translate(row):
     row = ["■" if e == SQUARE else e for e in row]
     row = ["⛌" if e == CROSS else e for e in row]
@@ -31,6 +33,28 @@ class Nanogram:
     field = [[v] + translate(row) for v, row in zip(self.vertical, self.board)]
     return tabulate(field, headers=self.horizontal, tablefmt="fancy_grid", stralign="center")
 
+  def _check(self, speile, info, offset, row):
+      # if info is 1 2 1
+      # there needs to be at least a cross in the interior gaps: 1 x 2 x 1
+      mask = [KEINE] + [SQUARE for _ in range(len(info) -1 )] + [KEINE]
+      balls = len(speile) - sum(mask) - sum(info)
+      boxes = len(mask)
+      common_square = [  1 for _ in speile]
+      common_cross  = [ -1 for _ in speile]
+      for dist in distribution(boxes, balls):
+        dist = [ di + mi for di, mi in zip(dist, mask)]
+        data = [[CROSS] * d + [SQUARE] * i for i, d in zip(info, dist)]
+# [[CROSS] * dist[0]]
+        print(info, dist, data)
+        if not self.compare_speile(data, speile):
+          continue 
+        for i in range(len(data)):
+          common_square[i] &= data[i] if data[i] == SQUARE else 0
+          if common_cross[i] == -1:
+            common_cross[i] = -1 if data[i] == -1 else 0 
+      self.set(offset, [cx + cs for cx, cs in zip(common_cross, common_square)], row=row)
+      # self.pprint()
+
   def step(self):
     for y, row in enumerate(self.board):
       self._check(row, self.vertical[y], y, row=True)
@@ -38,33 +62,6 @@ class Nanogram:
       temp = [self.board[y][x] for y in range(len(self.board))]
       self._check(temp, self.horizontal[x], x, row=False)
     
-  def _check(self, speile, info, offset, row):
-      mask = [KEINE] + [SQUARE for _ in range(len(info) -1 )] + [KEINE]
-      ball_in_mask = sum(mask)
-      ball_left = len(speile) - ball_in_mask - sum(info)
-      boxes = len(mask)
-      dists =  distribution(boxes, ball_left)
-      
-      overall_data_square = [ 1 for i in range(len(speile) )]
-      overall_data_cross = [ -1 for i in range(len(speile) )]
-      overall_data = [ 0 for i in range(len(speile) )]
-      for d in dists:
-        d = [ d[i] + mask[i] for i in range(len(d))]
-        data = [CROSS] * d[0]
-        for ih, h in enumerate(info):
-          data.extend([SQUARE] * h)
-          data.extend([CROSS] * d[ih+1])
-        if not self.compare_speile(data, speile):
-          continue 
-        # data += [CROSS] * d[-1]
-        for i in range(len(data)):
-          overall_data_square[i] &= data[i] if data[i] == SQUARE else 0
-          if overall_data_cross[i] == -1:
-            overall_data_cross[i] = -1 if data[i] == -1 else 0 
-      for i in range(len(overall_data_cross)):
-        overall_data[i] = overall_data_cross[i] + overall_data_square[i]
-      self.set(offset, overall_data,row=row)
-      self.pprint()
 
   def pprint(self):
       print(chr(27) + "[2J" + str(self), end='\n\r')
