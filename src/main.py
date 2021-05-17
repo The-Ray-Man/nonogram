20b35b6ea83ffe2441149edda71ea3e9ec5d3b5c
Gottschali <aligottschall22@gmail.com>
2021-05-17 22:29:07 +0200

    Allow variadic puzzle file arguments

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ import click
 from nanogram import Nanogram
 
 @click.command()
-@click.argument('puzzle', default="../examples/piggy.toml", type=click.File('rt'))
+@click.argument('puzzles', nargs=-1, type=click.File('rt'))
 @click.option("-s", "--sleep", default=0.1)
-def solve(puzzle, sleep):
+def solve(puzzles, sleep):
+  for puzzle in puzzles:
     N = Nanogram.load(puzzle, sleep=sleep)
     N.solve()
 
