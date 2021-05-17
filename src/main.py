0d7b8a0fcc6b1d43cf40f0ad94656d61bb2e3dda
Gottschali <aligottschall22@gmail.com>
2021-05-17 20:03:33 +0200

    remove unnecesary padding

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from nanogram import Nanogram
 @click.argument('puzzle', default="../examples/piggy.toml", type=click.File('rt'))
 @click.option("-s", "--sleep", default=0.1)
 def solve(puzzle, sleep):
-    print(puzzle, sleep)
     N = Nanogram.load(puzzle, sleep=sleep)
     N.solve()
 
--- src/nanogram.py
+++ src/nanogram.py
@@ -16,50 +16,36 @@ class Nanogram:
     return cls(data["horizontal"], data["vertical"], **kwargs)
 
   def __init__(self, horizontal, vertical, **kwargs):
-    self.horizontal = [[0]] + horizontal + [[0]]
-    self.vertical = [[0]] + vertical  + [[0]]
+    self.horizontal = horizontal
+    self.vertical = vertical
     self.board = [[KEINE for v in range(len(self.vertical))] for h in range(len(self.horizontal))]
     self.sleep = kwargs["sleep"] or SLEEP
-    self.pre_dists = {
-      "ROW": [None for _ in self.board],
-      "COL": [None for _ in self.board],
-    }
-    for i in range(len(self.board[0])):
-      self.board[0][i] = CROSS
-      self.board[-1][i] = CROSS
-    for i in range(len(self.board)):
-      self.board[i][0] = CROSS
-      self.board[i][-1] = CROSS
+    self._old = None
 
   def __str__(self):
-    # trans = str.maketrans({SQUARE: "■", KEINE: "", CROSS: "⛌"})
-    field = [[self.vertical[y]] + self.board[y][1:-1] for y in range(1, len(self.board) - 1)]
+    field = [[self.vertical[y]] + self.board[y] for y in range(len(self.board))]
     field = [["■" if e == SQUARE else e for e in row] for row in field]
     field = [["⛌" if e == CROSS else e for e in row] for row in field]
     field = [["" if e == KEINE else e for e in row] for row in field]
-    return tabulate(field, headers=self.horizontal[1:-1], tablefmt="fancy_grid", stralign="center")
+    return tabulate(field, headers=self.horizontal, tablefmt="fancy_grid", stralign="center")
 
-  def check(self):
+  def step(self):
     for y, row in enumerate(self.board):
-      if y==0 or y==len(self.board)-1:
-        continue
       self._check(row, self.vertical[y], y, row=True)
     for x in range(len(self.board)):
-      if x==0 or x==len(self.board)-1:
-        continue
       temp = [self.board[y][x] for y in range(len(self.board))]
       self._check(temp, self.horizontal[x], x, row=False)
     
   def _check(self, speile, info, offset, row):
       mask = [KEINE] + [SQUARE for _ in range(len(info) -1 )] + [KEINE]
       ball_in_mask = sum(mask)
-      ball_left = len(speile) - 2 - ball_in_mask - sum(info)
+      ball_left = len(speile) - ball_in_mask - sum(info)
       boxes = len(mask)
       dists =  distribution(boxes, ball_left)
       
-      overall_data_square = [ 1 for i in range(len(speile) - 2)]
-      overall_data_cross = [ -1 for i in range(len(speile) - 2)]
-      overall_data = [ 0 for i in range(len(speile) - 2)]
+      overall_data_square = [ 1 for i in range(len(speile) )]
+      overall_data_cross = [ -1 for i in range(len(speile) )]
+      overall_data = [ 0 for i in range(len(speile) )]
       for d in dists:
         d = [ d[i] + mask[i] for i in range(len(d))]
         data = [CROSS] * d[0]
@@ -84,12 +70,15 @@ class Nanogram:
 
   @staticmethod
   def compare_speile(data, speile):
-    for i_data, i_speile in zip(data, speile[1:-1]):
+    for i_data, i_speile in zip(data, speile):
       if i_data != KEINE and i_speile != KEINE and i_speile != i_data:
         return False
     return True
 
   def done(self):
+    if self.hash() == self._old:
+      return True
+    self._old = self.hash()
     for line in self.board:
       for x in line:
         if x == KEINE:
@@ -97,7 +86,6 @@ class Nanogram:
     return True
 
   def set(self, offset, speile, row=True):
-    speile = [CROSS] + speile + [CROSS]
     if row:
       self.board[offset] = speile
     else:
@@ -108,20 +96,6 @@ class Nanogram:
     return "".join(map(str, self.board))
 
   def solve(self):
-    old = self.hash()
-    while True:
-      self.check()
-      if self.hash() == old:
-        break
-      old = self.hash()
-      if self.done():
-        break
+    while not self.done():
+      self.step()
 
-
-
-
-if __name__ == "__main__":
-  N = Nanogram(
-    [[0],[1,3],[5],[2,2],[5],[1,1],[0]],
-        [[0],[4],[4],[2,1],[5],[4],[0]])
-  N.solve()
