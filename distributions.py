4254c2885c98472525c44d3b1f6b825abc3c985e
Gottschali <aligottschall22@gmail.com>
2021-05-17 15:33:03 +0200

    Cleanup distributions

--- distributions.py
+++ distributions.py
@@ -2,18 +2,28 @@ import pickle
 
 DUMP_FILE = "precomputation.pyb"
 
-def _distribution(boxes, balls):
+"""
+Precomputes all possible distributions and serializes and stores the resulting hashmap in a file.
+The file will be loaded during startup and queries will be looked up.
+"""
+
+def add_ball_to_boxes(bucket):
+    new_bucket = set()
+    for b in bucket:
+        for i in range(len(b)):
+            n = list(b)
+            n[i] += 1
+            new_bucket.add(tuple(n))
+    return new_bucket
+
+def _distribution(boxes, max_balls):
+    """ Computes all possiblities to distribute 0-max_balls into boxes """
     table = {}
-    bucket = {tuple(0 for i in range(boxes))}
-    for x in range(balls + 1):
-        new_bucket = set()
-        for b in bucket:
-            for i in range(len(b)):
-                n = list(b)
-                n[i] += 1
-                new_bucket.add(tuple(n))
-        temp = bucket.copy()
-        table[x] = temp
+    bucket = {tuple(0 for _ in range(boxes))}
+    table[0] = bucket.copy()
+    for ball in range(1, max_balls + 1):
+        new_bucket = add_ball_to_boxes(bucket)
+        table[ball] = new_bucket.copy()
         bucket = new_bucket
     return table
 
@@ -27,11 +37,10 @@ def dump(table):
         return pickle.dump(table, f)
 
 # TODO find out what is needed for what board sizes in the worst case
-# 10, 20
-def precompute(boxes, balls):
+def precompute(max_boxes, max_balls):
     table = {}
-    for b in range(boxes + 1):
-        table[b] = _distribution(b, balls)
+    for box in range(max_boxes + 1):
+        table[box] = _distribution(box, max_balls)
     return dump(table)
 
 lookup = load()
