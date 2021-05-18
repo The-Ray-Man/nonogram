b7b6cb1ff050ef4df9add565e6e0cfb96deae4be
Gottschali <aligottschall22@gmail.com>
2021-05-18 18:14:35 +0200

    Atleast one square in each speile

--- src/generator.py
+++ src/generator.py
@@ -1,10 +1,16 @@
-from random import choice
+from random import choice, shuffle
 
 from board import Board
 
 rng = lambda: choice([-1, 1])
 
+# Experiment with distributions
 class RandomBoard(Board):
     def __init__(self, size):
-        data = [[rng() for x in range(size[0])] for y in range(size[1])]
+        x, y = size
+        def row():
+            row = [1] + [rng() for x in range(x - 1)]
+            shuffle(row)
+            return row
+        data = [row() for y in range(y)]
         super().__init__(size, data)
