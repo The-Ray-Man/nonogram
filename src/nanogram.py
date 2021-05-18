749189d310e5dc5b2ca2189add31edb1d05501a6
Gottschali <aligottschall22@gmail.com>
2021-05-18 11:37:54 +0200

    Outsource board

--- src/nanogram.py
+++ src/nanogram.py
@@ -4,27 +4,9 @@ import toml
 
 from distributions import distribution
 from constants import *
+from board import Board
 
 # TODO verify function
-
-class Board(list):
-  def __init__(self, size, data=None):
-    w, h = size
-    for y in range(h):
-      if data is None:
-        self.append([KEINE for x in range(w)])
-      else:
-        self.append(data[y])
-
-  def done(self):
-    return all(i != KEINE for line in self for i in line)
-
-  def set(self, offset, speile, row=True):
-    if row:
-      self[offset] = speile
-    else:
-      for b, s in zip(self, speile):
-        b[offset] = s
 
 
 class Puzzle:
